24d2c24fb041c8fabd3cf4d94b70953f721925b9
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-07-02 19:55:50 +0600

    create page feadback

--- resources/views/feadback.blade.php
+++ resources/views/feadback.blade.php
@@ -1,14 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container bg-info">
-        <div class="row">
+    <div class="container-fluid">
+        <div class="row py-4">
             <div class="col-3">
                 @include('partials.left_sidebar')
             </div>
             <div class="col">
-                <h1>HGello</h1>
+                <div class="container">
+                    <div class="row bg-white p-3">
+                        <div class="col-5">
+                            <h1>Обратная связь</h1>
+                            <form>
+                                <div class="form-group">
+                                    <label for="exampleFormControlInput1">Электронная почта</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                           placeholder="name@example.com">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleFormControlTextarea1">Описание</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                </div>
+                                <button type="button" class="btn btn-primary">Отправить</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
+
 @endsection
