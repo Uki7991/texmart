24e474be828d7b7e8270cd5fa739d206265d3574
Uki7991 <tilek.kubanov@gmail.com>
2019-07-03 18:05:00 +0600

    Update left_sidebar.blade.php

--- resources/views/partials/left_sidebar.blade.php
+++ resources/views/partials/left_sidebar.blade.php
@@ -1,22 +1,75 @@
-<div class="">
-    <div class="">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a href="#" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
+{{--<div class="">--}}
+{{--    <div class="">--}}
+{{--        <ul class="nav flex-column">--}}
+{{--            <li class="nav-item">--}}
+{{--                <a href="#" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">--}}
+{{--                    <span class="col m-0 h5 font-weight-light">Мужские</span>--}}
+{{--                    <span class="col-auto">--}}
+{{--                        <img src="{{ asset('icons/boss.png') }}" class="img-fluid pl-2 border-left" width="60" height="60" alt="">--}}
+{{--                    </span>--}}
+{{--                </a>--}}
+{{--            </li>--}}
+{{--            <li class="nav-item">--}}
+{{--                <a href="#" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">--}}
+{{--                    <span class="col m-0 h5 font-weight-light">Женские</span>--}}
+{{--                    <span class="col-auto">--}}
+{{--                        <img src="{{ asset('icons/woman.png') }}" class="img-fluid pl-2 border-left" width="60" height="60" alt="">--}}
+{{--                    </span>--}}
+{{--                </a>--}}
+{{--            </li>--}}
+{{--        </ul>--}}
+{{--    </div>--}}
+{{--</div>--}}
+
+
+<div class="accordion" id="accordionExample">
+    <div class="card border-0 bg-white">
+        <div class="card-header  border-0 bg-white p-0" id="headingOne">
+            <h2 class="mb-0">
+                <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
                     <span class="col m-0 h5 font-weight-light">Мужские</span>
                     <span class="col-auto">
                         <img src="{{ asset('icons/boss.png') }}" class="img-fluid pl-2 border-left" width="60" height="60" alt="">
                     </span>
                 </a>
-            </li>
-            <li class="nav-item">
-                <a href="#" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
+            </h2>
+        </div>
+
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+        </div>
+    </div>
+    <div class="card border-0 bg-white">
+        <div class="card-header border-0 bg-white p-0" id="headingTwo">
+            <h2 class="mb-0">
+                <a href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"  class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
                     <span class="col m-0 h5 font-weight-light">Женские</span>
                     <span class="col-auto">
                         <img src="{{ asset('icons/woman.png') }}" class="img-fluid pl-2 border-left" width="60" height="60" alt="">
                     </span>
                 </a>
-            </li>
-        </ul>
+            </h2>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+            <div class="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header" id="headingThree">
+            <h2 class="mb-0">
+                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Collapsible Group Item #3
+                </button>
+            </h2>
+        </div>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+            <div class="card-body">
+                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+            </div>
+        </div>
     </div>
 </div>
