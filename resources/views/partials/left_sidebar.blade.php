1c504d8fa583c4d8973dc2ea61952afd7a4f4424
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-07-03 18:28:13 +0600

    create children category

--- resources/views/partials/left_sidebar.blade.php
+++ resources/views/partials/left_sidebar.blade.php
@@ -58,12 +58,15 @@
             </div>
         </div>
     </div>
-    <div class="card">
-        <div class="card-header" id="headingThree">
+    <div class="card border-0 bg-white">
+        <div class="card-header border-0 bg-white p-0" id="headingThree">
             <h2 class="mb-0">
-                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
-                </button>
+                <a href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo"  class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
+                    <span class="col m-0 h5 font-weight-light">Детские</span>
+                    <span class="col-auto">
+                        <img src="{{ asset('icons/baby.png') }}" class="img-fluid pl-2 border-left" width="60" height="60" alt="">
+                    </span>
+                </a>
             </h2>
         </div>
         <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
