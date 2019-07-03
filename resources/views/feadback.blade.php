f9a4d92b4e8c6e844d0d993a891a6d04133f8fb8
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-07-03 11:07:17 +0600

    update feedback page

--- resources/views/feadback.blade.php
+++ resources/views/feadback.blade.php
@@ -11,11 +11,24 @@
                     <div class="row bg-white p-3">
                         <div class="col">
                             <h2>Консультация</h2>
-                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at cum cupiditate earum eius esse eveniet facere fugiat harum illum laborum, libero molestias mollitia pariatur quisquam, repudiandae sapiente vitae voluptates.</p>
+                            <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at
+                                cum cupiditate earum eius esse eveniet facere fugiat harum illum laborum, libero
+                                molestias mollitia pariatur quisquam, repudiandae sapiente vitae voluptates.</p>
                         </div>
                         <div class="col-5">
                             <h1>Обратная связь</h1>
                             <form>
+                                <div class="form-row">
+                                    <div class="col">
+                                        <label for="firstName">Ваше имя</label>
+                                        <input type="text" class="form-control" id="firstName" placeholder="Иван">
+                                    </div>
+                                    <div class="col">
+                                        <label for="firstName">Фамилия</label>
+                                        <input type="text" class="form-control" id="lastName" placeholder="Медведев">
+                                    </div>
+                                </div>
+
                                 <div class="form-group">
                                     <label for="exampleFormControlInput1">Электронная почта</label>
                                     <input type="email" class="form-control" id="exampleFormControlInput1"
