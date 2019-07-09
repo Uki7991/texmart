d218af8be1969ea5460c56d909f1b15512d07e3c
Uki7991 <tilek.kubanov@gmail.com>
2019-07-09 17:02:23 +0600

    wdwad

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -4,50 +4,7 @@
 
     <div class="container">
         <div class="row py-4">
-            <div class="col">
-                <div class="container bg-white py-3">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <div class="list-group">
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                                <a class="list-group-item text-dark" href="">Item</a>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="card">
-                                <div class="card-body conversations">
-                                    <div class="row">
-                                        <div class="col-md-10">
-                                            <p>
-                                                <strong>asd</strong><br>
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <form action="" method="post">
-                                        <div class="form-group">
-                                            <textarea name="content" cols="" rows="20" placeholder="Напишите сообщение"
-                                                      class="form-control"></textarea>
-                                        </div>
-                                        <button class="btn btn-primary" type="submit">Отправить</button>
-                                    </form>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @include('partials.chat')
         </div>
     </div>
 @endsection
---
+++ resources/views/partials/chat.blade.php
@@ -0,0 +1,38 @@
+<div class="col-2">
+    <div class="list-group">
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+        <a class="list-group-item text-dark" href="">Item</a>
+    </div>
+</div>
+
+<div class="col">
+    <div class="card">
+        <div class="card-body conversations">
+            <div class="row">
+                <div class="col-md-10">
+                    <p>
+                        <strong>asd</strong><br>
+                    </p>
+                </div>
+            </div>
+            <hr>
+            <form action="" method="post">
+                <div class="form-group">
+                                <textarea name="content" cols="" rows="20" placeholder="Напишите сообщение"
+                                          class="form-control"></textarea>
+                </div>
+                <button class="btn btn-primary" type="submit">Отправить</button>
+            </form>
+        </div>
+    </div>
+</div>
