ff0e0d44f8d12a69c7f6a2c96aa3ea8e98dfe10e
Uki7991 <tilek.kubanov@gmail.com>
2019-07-09 14:09:15 +0600

    Update left_sidebar.blade.php

--- resources/views/partials/left_sidebar.blade.php
+++ resources/views/partials/left_sidebar.blade.php
@@ -1,29 +1,5 @@
-{{--<div class="">--}}
-{{--    <div class="">--}}
-{{--        <ul class="nav flex-column">--}}
-{{--            <li class="nav-item">--}}
-{{--                <a href="#" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">--}}
-{{--                    <span class="col m-0 h5 font-weight-light">Мужские</span>--}}
-{{--                    <span class="col-auto">--}}
-{{--                        <img src="{{ asset('icons/boss.png') }}" class="img-fluid pl-2 border-left" width="60" height="60" alt="">--}}
-{{--                    </span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--            <li class="nav-item">--}}
-{{--                <a href="#" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">--}}
-{{--                    <span class="col m-0 h5 font-weight-light">Женские</span>--}}
-{{--                    <span class="col-auto">--}}
-{{--                        <img src="{{ asset('icons/woman.png') }}" class="img-fluid pl-2 border-left" width="60" height="60" alt="">--}}
-{{--                    </span>--}}
-{{--                </a>--}}
-{{--            </li>--}}
-{{--        </ul>--}}
-{{--    </div>--}}
-{{--</div>--}}
-
-
 <div class="accordion" id="accordionExample">
-    <div class="card border-0 bg-white">
+    <div class="card border-bottom border-top-0 border-left-0 border-right-0 bg-white">
         <div class="card-header  border-0 bg-white p-0" id="headingOne">
             <h2 class="mb-0">
                 <a href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne" class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
@@ -61,7 +37,7 @@
             </div>
         </div>
     </div>
-    <div class="card border-0 bg-white">
+    <div class="card border-bottom border-top-0 border-left-0 border-right-0 bg-white">
         <div class="card-header border-0 bg-white p-0" id="headingTwo">
             <h2 class="mb-0">
                 <a href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"  class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
@@ -98,7 +74,7 @@
             </div>
         </div>
     </div>
-    <div class="card border-0 bg-white">
+    <div class="card border-bottom border-top-0 border-left-0 border-right-0 bg-white">
         <div class="card-header border-0 bg-white p-0" id="headingThree">
             <h2 class="mb-0">
                 <a href="#" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseTwo"  class="nav-link bg-primary text-light d-flex align-items-center px-0 border-bottom">
