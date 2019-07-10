883428a7206359de5f8e0893f314a63d5f470d50
Uki7991 <tilek.kubanov@gmail.com>
2019-07-10 15:56:48 +0600

    Create profile.blade.php

---
+++ resources/views/vendor/voyager/profile.blade.php
@@ -0,0 +1,44 @@
+@extends('voyager::master')
+
+@section('css')
+@stop
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row py-4">
+            <div class="col">
+                <div class="container bg-white py-3">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist"
+                                 aria-orientation="vertical">
+                                <a class="nav-link active" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile"
+                                   role="tab" aria-controls="v-pills-profile" aria-selected="true">Личный кабинет</a>
+                                <a class="nav-link" id="v-pills-announcement-tab" data-toggle="pill" href="#v-pills-announcement"
+                                   role="tab" aria-controls="v-pills-announcement" aria-selected="false">Объявления</a>
+                                <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill"
+                                   href="#v-pills-messages" role="tab" aria-controls="v-pills-messages"
+                                   aria-selected="false">Сообщения</a>
+                                <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill"
+                                   href="#v-pills-settings" role="tab" aria-controls="v-pills-settings"
+                                   aria-selected="false">Settings</a>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="tab-content" id="v-pills-tabContent">
+                                @include('profile.tabs.edit_profile')
+                                @include('profile.tabs.add_announcement')
+                                @include('profile.tabs.message')
+                                <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
