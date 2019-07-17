f66df06a18b50b6f577cbaed58588b1db52c88d8
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-07-17 17:30:31 +0600

    вап

--- resources/views/partials/feature.blade.php
+++ resources/views/partials/feature.blade.php
@@ -3,7 +3,7 @@
         <div class="card border-0">
             <img src="{{ asset('img/traffic.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Перевозки</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
                     additional content. This content is a little bit longer.</p>
             </div>
@@ -11,7 +11,7 @@
         <div class="card border-0">
             <img src="{{ asset('img/woman with telephone.jpg') }}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">Консультации</h5>
                 <p class="card-text">This card has supporting text below as a natural lead-in to additional
                     content.</p>
             </div>
