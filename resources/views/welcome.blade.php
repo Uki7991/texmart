93ff1152d56e1f2c07c047902858bdea9f1be06c
Uki7991 <tilek.kubanov@gmail.com>
2019-07-23 16:43:51 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,20 +31,16 @@
     </div>
     <div class="container-fluid">
         <div class="row justify-content-center py-4">
-            <main class="col-8">
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <h2 class="font-weight-bold text-underline pb-3">Продукции и цеха</h2>
-                    </div>
-                    <div class="col-12 mt-3">
-                        @include('productions.list')
-                    </div>
-                    <div class="col-12 mt-3 text-center">
-                        <a href="{{ route('productions.index') }}" class="btn text-dark text-dotted rounded-0">Больше
-                            продукций...</a>
-                    </div>
-                </div>
-            </main>
+            <div class="col-12 text-center">
+                <h2 class="font-weight-bold text-underline pb-3">Продукции и цеха</h2>
+            </div>
+            <div class="col-12 mt-3">
+                @include('productions.list')
+            </div>
+            <div class="col-12 mt-3 text-center">
+                <a href="{{ route('productions.index') }}" class="btn text-dark text-dotted rounded-0">Больше
+                    продукций...</a>
+            </div>
         </div>
     </div>
     <div>
