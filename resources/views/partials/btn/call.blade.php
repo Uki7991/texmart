0ab699f0902bca225a2aef9a978b61e9039eff03
Uki7991 <tilek.kubanov@gmail.com>
2019-07-24 15:55:33 +0600

    buttons

--- resources/views/partials/btn/call.blade.php
+++ resources/views/partials/btn/call.blade.php
@@ -1,3 +1,3 @@
-<a href="{{ $route ?? '#' }}" class="btn {{ isset($bootstrap3) && $bootstrap3 ? 'btn-primary' : '' }} border border-dark shadow-sm rounded-pill mx-1 call-btn transition-100 {{ $class ?? '' }}">
-    Связаться <i class="fas fa-phone text-danger"></i>
+<a href="{{ $route ?? '#' }}" class="btn {{ isset($bootstrap3) && $bootstrap3 ? 'btn-primary' : '' }} shadow-sm rounded-pill mx-1 call-btn transition-100 {{ $class ?? '' }}">
+    <i class="fas fa-envelope text-success"></i>
 </a>
--- resources/views/productions/single.blade.php
+++ resources/views/productions/single.blade.php
@@ -3,13 +3,13 @@
         <div class="card-img-top position-relative">
             <img src="{{ asset('img/2 lg.jpg') }}" class="img-fluid img-responsive" alt="">
         </div>
-        <div class="card-body">
+        <div class="card-body pb-0">
             <h6 class="font-weight-bold card-title">{{ Str::limit($production->title ?? 'ОсОО "Швея на час"', 18) }}</h6>
-            <p class="card-text">
-                {{ Str::limit($production->excerpt ?? 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum cumque dignissimos eligendi, est hic impedit iure laboriosam laudantium molestiae, molestias nulla odit quae quaerat quibusdam recusandae repellendus tempora voluptatem.', 80, '...') }}
-            </p>
+{{--            <p class="card-text">--}}
+{{--                {{ Str::limit($production->excerpt ?? 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis cum cumque dignissimos eligendi, est hic impedit iure laboriosam laudantium molestiae, molestias nulla odit quae quaerat quibusdam recusandae repellendus tempora voluptatem.', 80, '...') }}--}}
+{{--            </p>--}}
         </div>
-        <div class="card-footer d-flex justify-content-around">
+        <div class="card-footer d-flex justify-content-end bg-white border-0 pt-0">
             @include('partials.btn.call', ['class' => 'btn-sm'])
             @include('partials.btn.favorite', ['class' => 'btn-sm', 'data' => 'data-id='.$production->id.'', 'route' => \Illuminate\Support\Facades\Auth::user() ? '#' : route('login')])
         </div>
