8559f5e787d425234ac3befdadcb43e8fafdda30
uki7991 <tilek.kubanov@gmail.com>
2019-08-15 12:57:19 +0600

    fix

--- resources/views/partials/feature.blade.php
+++ resources/views/partials/feature.blade.php
@@ -30,37 +30,37 @@
             <div class="border border-texmart-orange rounded-pill p-2 feature mr-2">
                 <img src="{{ asset('icons/clipboard.png') }}" class="mx-auto feature-logo" alt="...">
             </div>
-            <p class="small pt-2">Единная база производителей</p>
+            <p class="pt-2 font-weight-bold">Единная база производителей</p>
         </div>
         <div class="col d-flex align-items-center">
             <div class="border border-texmart-orange rounded-pill p-2 feature mr-2">
                 <img src="{{ asset('icons/sewing (1).png') }}" class="mx-auto feature-logo" alt="...">
             </div>
-            <p class="small pt-2">Производсво одежды в КР</p>
+            <p class="pt-2 font-weight-bold">Производсво одежды в КР</p>
         </div>
         <div class="col d-flex align-items-center">
             <div class="border border-texmart-orange rounded-pill p-2 feature mr-2">
                 <img src="{{ asset('icons/medal.png') }}" class="mx-auto feature-logo" alt="...">
             </div>
-            <p class="small pt-2">Оценка качества</p>
+            <p class="pt-2 font-weight-bold">Оценка качества</p>
         </div>
         <div class="col d-flex align-items-center">
             <div class="border border-texmart-orange rounded-pill p-2 feature mr-2">
                 <img src="{{ asset('icons/fast-delivery.png') }}" class="mx-auto feature-logo" alt="...">
             </div>
-            <p class="small pt-2">Доставка</p>
+            <p class="pt-2 font-weight-bold">Доставка</p>
         </div>
         <div class="col d-flex align-items-center">
             <div class="border border-texmart-orange rounded-pill p-2 feature mr-2">
                 <img src="{{ asset('icons/knitting.png') }}" class="mx-auto feature-logo" alt="...">
             </div>
-            <p class="small pt-2">Поставка тканей и фурнитуры</p>
+            <p class="pt-2 font-weight-bold">Поставка тканей и фурнитуры</p>
         </div>
         <div class="col d-flex align-items-center">
             <div class="border border-texmart-orange rounded-pill p-2 feature mr-2">
                 <img src="{{ asset('icons/contract (2).png') }}" class="mx-auto feature-logo" alt="...">
             </div>
-            <p class="small pt-2">Оформление документов</p>
+            <p class="pt-2 font-weight-bold">Оформление документов</p>
         </div>
     </div>
 </div>
