24c02be13fe54a4d8579e988cbafef96faa68a67
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-08-15 13:22:07 +0600

    fx footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -16,14 +16,39 @@
             <a class="nav-link text-dark small" href="#"><i class="fa fa-angle-double-right"></i> Link</a>
         </nav>
     </div>
-    <div class="row">
+    <div class="col-12 col-sm-4 ml-auto col-md-3">
         <h5>Наши Контакты</h5>
-        <div class="col-md-12 col-sm-4">
-            <div class="address blocks">
+        <ul class="nav flex-column">
+            <li class="nav-item ">
+                <a href="" class="nav-link text-dark small d-flex ">
+                    <i class="fas fa-phone p-1"></i>&nbsp;
+                    <div>
+                        <p class="mb-1 font-weight-bold">07009345345</p>
+                        <p class="m-0 text-muted">Пн. – Пт.: с 9:00 до 18:00 (+6 GMT)</p>
+                    </div>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a href="" class="nav-link text-dark small d-flex ">
+                    <i class="fas fa-map p-1"></i>&nbsp;
+                    <div>
+                        <p class="mb-1 font-weight-bold">Кыргызстан, г. Бишкек
+                            ул. Тоголока Молдо 104</p>
+                    </div>
+                </a>
+            </li>
+            <li class="nav-item ">
+                <a href="" class="nav-link text-dark small d-flex ">
+                    <i class="fas fa-envelope p-1"></i>&nbsp;
+                    <div>
+                        <p class="mb-1 font-weight-bold">support@texmart.kg</p>
+                    </div>
+                </a>
+            </li>
 
-            </div>
-        </div>
+        </ul>
 
 
     </div>
+
 </div>
