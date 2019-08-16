9942f53aea3696c796821c81ad8001b8d61570b3
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-08-17 02:48:25 +0600

    asd

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -5,16 +5,14 @@
     <div class="container">
         <div class="row py-4">
             <div class="col">
-                <div class="container">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolorum et eveniet, ex minus nemo
-                        obcaecati odio odit, provident quasi sapiente sit temporibus tenetur ut velit, veniam voluptate!
-                        Aut cum doloribus laboriosam natus suscipit? Fugit iure labore perspiciatis sequi tenetur!</p>
-                </div>
                 <div>
                     <p>Офис компании Texmart.kg находится в г. Бишкек. Texmart.kg является информационным порталом, объединяющий текстильную промышленность Кыргызстана.
-                        На этом портале Вы найдете долгосрочного партнера в швейно-текстильной сфере!</p>
+                        На этом портале Вы найдете долгосрочного партнера в швейно-текстильной сфере!
+                    </p>
                 </div>
-                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A95d8395382f6f742cb4d5dca4a8aa599cc96f478858d19aa2106e15d67bd6cd0&amp;source=constructor" width="700" height="400" frameborder="0"></iframe>
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A95d8395382f6f742cb4d5dca4a8aa599cc96f478858d19aa2106e15d67bd6cd0&amp;source=constructor" width="700" height="400" frameborder="0">
+
+                </iframe>
             </div>
             <div class="flex-column">
                 <table>
