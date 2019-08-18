aff03a306eeeb7d800717c4b60796dfac2d6da02
Uki7991 <tilek.kubanov@gmail.com>
2019-08-18 21:57:46 +0600

    fix

--- resources/views/partials/promo.blade.php
+++ resources/views/partials/promo.blade.php
@@ -15,19 +15,19 @@
     <div class="promo-carousel owl-carousel owl-theme">
         <div class="item">
             <img src="img/odezhda-veshalka-ruka.jpg" class="img-fluid" alt="...">
-            <div class="position-absolute text-dark font-weight-bold bg-white w-50 p-5 text-center promo-carousel-text">
+            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">
                 <h2>Первая интернет платформа швейной и текстильной промышленности Кыргызстана</h2>
             </div>
         </div>
         <div class="item">
             <img src="img/business-document-signing.jpg" class="img-fluid" alt="...">
-            <div class="position-absolute text-dark font-weight-bold bg-white w-50 p-5 text-center promo-carousel-text">
+            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">
                 <h2>Консалтинг, оформление всех<br>бухгалтерских и юридических документов</h2>
             </div>
         </div>
         <div class="item">
             <img src="img/867519228d1d5325856fc61d710ded0e_XL.jpg" class="img-fluid" alt="...">
-            <div class="position-absolute text-dark font-weight-bold bg-white w-50 p-5 text-center promo-carousel-text">
+            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">
                 <h2>Оценка качества пошива одежды и <br>производства от <span class="text-uppercase">Texmart</span></h2>
             </div>
         </div>
