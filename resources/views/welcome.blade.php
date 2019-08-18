2eed2eae3b1268c1ce2c083cfc051131620d85c3
Uki7991 <tilek.kubanov@gmail.com>
2019-08-18 17:48:44 +0600

    fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -137,6 +137,9 @@
     <script>
         $('.owl-carousel').owlCarousel({
             loop: true,
+            autoplay: true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
             margin: 10,
             nav: true,
             responsive: {
