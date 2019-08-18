6795e0fdf8734c099e944e4fab864a5fa4a78683
Uki7991 <tilek.kubanov@gmail.com>
2019-08-18 19:41:07 +0600

    Update promo.blade.php

--- resources/views/partials/promo.blade.php
+++ resources/views/partials/promo.blade.php
@@ -14,13 +14,13 @@
 
     <div class="promo-carousel owl-carousel owl-theme">
         <div class="item">
-            <img src="img/odezhda-veshalka-ruka.jpg" class="d-block w-100" alt="...">
+            <img src="img/odezhda-veshalka-ruka.jpg" class="img-fluid" alt="...">
         </div>
         <div class="item">
-            <img src="img/business-document-signing.jpg" class="d-block w-100" alt="...">
+            <img src="img/business-document-signing.jpg" class="img-fluid" alt="...">
         </div>
         <div class="item">
-            <img src="img/867519228d1d5325856fc61d710ded0e_XL.jpg" class="d-block w-100" alt="...">
+            <img src="img/867519228d1d5325856fc61d710ded0e_XL.jpg" class="img-fluid" alt="...">
         </div>
     </div>
 </div>
