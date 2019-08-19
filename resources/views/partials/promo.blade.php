c616442bbfbc6852b71317ff7c19827b4a511141
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-08-19 23:35:17 +0600

    fx carousel

--- resources/views/partials/promo.blade.php
+++ resources/views/partials/promo.blade.php
@@ -14,22 +14,22 @@
 
     <div class="promo-carousel owl-carousel owl-theme">
         <div class="item">
-            <img src="img/odezhda-veshalka-ruka.jpg" class="img-fluid" alt="...">
-            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">
-                <h2>Первая интернет платформа швейной и текстильной промышленности Кыргызстана</h2>
-            </div>
+            <img src="img/photo_2019-08-19_23-26-44.jpg" class="img-fluid" alt="...">
+{{--            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">--}}
+{{--                <h2>Первая интернет платформа швейной и текстильной промышленности Кыргызстана</h2>--}}
+{{--            </div>--}}
         </div>
         <div class="item">
-            <img src="img/business-document-signing.jpg" class="img-fluid" alt="...">
-            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">
-                <h2>Консалтинг, оформление всех<br>бухгалтерских и юридических документов</h2>
-            </div>
+            <img src="img/photo_2019-08-19_23-26-42.jpg" class="img-fluid" alt="...">
+{{--            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">--}}
+{{--                <h2>Консалтинг, оформление всех<br>бухгалтерских и юридических документов</h2>--}}
+{{--            </div>--}}
         </div>
         <div class="item">
-            <img src="img/867519228d1d5325856fc61d710ded0e_XL.jpg" class="img-fluid" alt="...">
-            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">
-                <h2>Оценка качества пошива одежды и <br>производства от <span class="text-uppercase">Texmart</span></h2>
-            </div>
+            <img src="img/photo_2019-08-19_23-26-33.jpg" class="img-fluid" alt="...">
+{{--            <div class="position-absolute text-dark font-weight-bold bg-white-75 w-100 p-5 text-center promo-carousel-text">--}}
+{{--                <h2>Оценка качества пошива одежды и <br>производства от <span class="text-uppercase">Texmart</span></h2>--}}
+{{--            </div>--}}
         </div>
     </div>
 </div>
