b46601b894209bc234c5c9a441e4be847076da53
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-08-20 14:00:45 +0600

    fx

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,8 +3,7 @@
         <h5>О компании</h5>
         <nav class="nav flex-column">
             <a class="nav-link text-dark small nav-scale" href="/about"><i class="fa fa-angle-double-right"></i> О нас</a>
-            <a class="nav-link text-dark small nav-scale" href="#"><i class="fa fa-angle-double-right"></i> Отзывы</a>
-            <a class="nav-link text-dark small nav-scale" href="#"><i class="fa fa-angle-double-right"></i> Преимущества</a>
+            <a class="nav-link text-dark small nav-scale" href="#"><i class="fa fa-angle-double-right"></i> Форум</a>
             <a class="nav-link text-dark small nav-scale" href="/contacts"><i class="fa fa-angle-double-right"></i> Контакты</a>
         </nav>
     </div>
@@ -13,7 +12,6 @@
         <nav class="nav flex-column">
             <a class="nav-link text-dark small nav-scale" href="/privacy"><i class="fa fa-angle-double-right"></i> Политика Конфидециальности</a>
             <a class="nav-link text-dark small nav-scale" href="/conditions"><i class="fa fa-angle-double-right"></i> Условия использования сайта</a>
-            <a class="nav-link text-dark small nav-scale" href="#"><i class="fa fa-angle-double-right"></i> Link</a>
         </nav>
     </div>
     <div class="col-12 col-sm-4 col-md-3 mb-4 mb-md-0">
