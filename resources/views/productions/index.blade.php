df567ddfd1308ea1c3591f25be7fb45d0ab0e6be
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-08-20 15:04:19 +0600

    fx quality

--- resources/views/productions/index.blade.php
+++ resources/views/productions/index.blade.php
@@ -6,7 +6,7 @@
 
     <div class="container-fluid py-4">
         <div class="row">
-            <div class="col-2">
+            <div class="col-">
                 @include('partials.left_sidebar', ['toggle' => false])
             </div>
             <div class="col">
