deb6ea7d46c68b0378a9ea8a1feaf69884bf1198
Uki7991 <tilek.kubanov@gmail.com>
2019-08-25 20:44:07 +0600

    аfx

--- resources/views/user-production/profile-tabs/settings.blade.php
+++ resources/views/user-production/profile-tabs/settings.blade.php
@@ -15,27 +15,6 @@
                     @enderror
                 </div>
                 <div class="form-group">
-                    <label for="password" class="col-form-label text-md-right"><i
-                            class="fas fa-key text-primary"></i> {{ __('Пароль') }}</label>
-                    <input id="password" type="password"
-                           class="form-control shadow-sm @error('password') is-invalid @enderror" name="password"
-                           required
-                           autocomplete="new-password">
-
-                    @error('password')
-                    <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="password-confirm" class=" col-form-label text-md-right"><i
-                            class="fas fa-key text-primary"></i> {{ __('Подтвердите пароль') }}</label>
-                    <input id="password-confirm" type="password" class="form-control shadow-sm"
-                           name="password_confirmation"
-                           required autocomplete="new-password">
-                </div>
-                <div class="form-group">
                     <label>
                         Картинка для аватарки
                     </label>
@@ -43,31 +22,32 @@
                 </div>
                 <button type="button" class="btn btn-success">Сохранить изменения</button>
             </div>
-        </div>
-        <div class="form-row">
             <div class="col-6">
                 <div class="form-group">
-                    <label for="password" class="col-form-label text-md-right"><i
+                    <label for="password" class="text-md-right"><i
                             class="fas fa-key text-primary"></i> {{ __('Пароль') }}</label>
                     <input id="password" type="password"
-                           class="form-control shadow-sm @error('password') is-invalid @enderror" name="password"
+                           class="form-control shadow-sm @error('password') is-invalid @enderror"
+                           name="password"
                            required
                            autocomplete="new-password">
 
                     @error('password')
-                    <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                </span>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
                     @enderror
                 </div>
                 <div class="form-group">
-                    <label for="password-confirm" class=" col-form-label text-md-right"><i
+                    <label for="password-confirm" class="text-md-right"><i
                             class="fas fa-key text-primary"></i> {{ __('Подтвердите пароль') }}</label>
                     <input id="password-confirm" type="password" class="form-control shadow-sm"
                            name="password_confirmation"
                            required autocomplete="new-password">
                 </div>
+                <button type="button" class="btn btn-success">Изменить пароль</button>
             </div>
         </div>
+
     </div>
 </div>
