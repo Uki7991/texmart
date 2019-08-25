71d8a7e707599c25e632b0de20fccf8cbe9caf7c
Uki7991 <tilek.kubanov@gmail.com>
2019-08-25 18:38:19 +0600

    Update single.blade.php

--- resources/views/productions/single.blade.php
+++ resources/views/productions/single.blade.php
@@ -1,6 +1,6 @@
 <div class="card border shadow-sm production-card transition-500">
     <a href="{{ route('productions.show', $production->slug) }}" class="text-dark text-decoration-none">
-        <div class="card-img-top position-relative">
+        <div class="card-img-top position-relative" style="min-height: 141px; max-height: 141px;">
             <img src="{{ $production->logo && file_exists('storage/'.$production->logo) ? asset('storage/'.$production->logo) : asset('img/2 lg.jpg') }}" class="img-fluid img-responsive" alt="">
         </div>
         <div class="card-body pb-0">
