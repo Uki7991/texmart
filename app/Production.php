7b75549d06e8467238c5442d303809069cb3f0ce
uki7991 <tilek.kubanov@gmail.com>
2019-08-27 00:20:52 +0600

    fix

--- app/Production.php
+++ app/Production.php
@@ -16,7 +16,8 @@ class Production extends Model
     use RateableTrait;
     use Spatial;
 
-    protected $fillable = ['title', 'address', 'excerpt', 'description', 'phone1', 'phone2', 'email', 'site', 'type', 'tools', 'amount_production', 'brand',];
+    protected $fillable = ['title', 'address', 'excerpt', 'description', 'phone1',
+        'phone2', 'email', 'site', 'type', 'tools', 'amount_production', 'brand',];
 
     protected $spatial = ['coordinates'];
     protected $casts = ['images'];
