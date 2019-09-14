002d90725b8b7d42d3a7c7f3119e1ab32e2a91ec
Uki7991 <tilek.kubanov@gmail.com>
2019-09-14 10:37:00 +0600

    fx

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,12 +9,12 @@
                     <div class="row">
                         <div class="col-md-auto col-2 bg-light rounded-left py-3 text-secondary">
                             <ul class="nav flex-column">
-                                <li class="nav-item my-2">
+                                <li class="nav-item d-none my-2">
                                     <a href="{{ route('google.redirect', 'facebook') }}" class="text-secondary">
                                         <i class="fab fa-facebook fa-lg"></i>
                                     </a>
                                 </li>
-                                <li class="nav-item my-2">
+                                <li class="nav-item d-none my-2">
                                     <a href="{{ route('google.redirect', 'vk') }}" class="text-secondary">
                                         <i class="fab fa-vk fa-lg"></i>
                                     </a>
@@ -24,12 +24,12 @@
                                         <i class="fab fa-google fa-lg"></i>
                                     </a>
                                 </li>
-                                <li class="nav-item my-2">
+                                <li class="nav-item d-none my-2">
                                     <a href="{{ route('google.redirect', 'twitter') }}" class="text-secondary">
                                         <i class="fab fa-twitter fa-lg"></i>
                                     </a>
                                 </li>
-                                <li class="nav-item my-2">
+                                <li class="nav-item d-none my-2">
                                     <a href="{{ route('google.redirect', 'odnoklassniki') }}" class="text-secondary">
                                         <i class="fab fa-odnoklassniki-square fa-lg"></i>
                                     </a>
