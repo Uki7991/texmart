4a4ba2e6ba8f287e9ba217e74df1bc42a8aa68c1
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-09-26 15:01:22 +0600

    fx

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,33 +2,33 @@
     <div class="col-12 col-sm-4 col-md-3 col-lg-2 col-xl-2 mb-4 mb-md-0">
         <h5>О компании</h5>
         <nav class="nav flex-column">
-            <a class="nav-link text-dark small nav-scale" href="/about"><i class="fa fa-angle-double-right"></i> О нас</a>
-            <a class="nav-link text-dark small nav-scale" href="#"><i class="fa fa-angle-double-right"></i> Форум</a>
-            <a class="nav-link text-dark small nav-scale" href="/blog"><i class="fa fa-angle-double-right"></i> Блог</a>
-            <a class="nav-link text-dark small nav-scale" href="/contacts"><i class="fa fa-angle-double-right"></i> Контакты</a>
+            <a class="nav-link text-dark small nav-scale" href="/about" title="О нас"><i class="fa fa-angle-double-right"></i> О нас</a>
+            <a class="nav-link text-dark small nav-scale" href="#" title="Форум"><i class="fa fa-angle-double-right"></i> Форум</a>
+            <a class="nav-link text-dark small nav-scale" href="/blog" title="Блог"><i class="fa fa-angle-double-right"></i> Блог</a>
+            <a class="nav-link text-dark small nav-scale" href="/contacts" title="контакты"><i class="fa fa-angle-double-right"></i> Контакты</a>
         </nav>
     </div>
     <div class="col-12 col-sm-4 col-md-3 mb-4 mb-md-0">
         <h5>Сервис и поддержка</h5>
         <nav class="nav flex-column">
-            <a class="nav-link text-dark small nav-scale" href="/privacy"><i class="fa fa-angle-double-right"></i> Политика Конфидециальности</a>
-            <a class="nav-link text-dark small nav-scale" href="/conditions"><i class="fa fa-angle-double-right"></i> Условия использования сайта</a>
-            <a class="nav-link text-dark small nav-scale" href="#"><i class="fa fa-angle-double-right"></i> Доставка</a>
+            <a class="nav-link text-dark small nav-scale" href="/privacy" title="Политика Конфидециальности"><i class="fa fa-angle-double-right"></i> Политика Конфидециальности</a>
+            <a class="nav-link text-dark small nav-scale" href="/conditions" title="Условия использования сайта"><i class="fa fa-angle-double-right"></i> Условия использования сайта</a>
+            <a class="nav-link text-dark small nav-scale" href="#" title="Доставка"><i class="fa fa-angle-double-right"></i> Доставка</a>
         </nav>
     </div>
     <div class="col-12 col-sm-4 col-md-3 mb-4 mb-md-0">
         <h5>Объявления</h5>
         <nav class="nav flex-column">
-            <a class="nav-link text-dark small nav-scale" href="{{ route('productions.index', ['type' => 'productions']) }}"><i class="fa fa-angle-double-right"></i> Производство</a>
-            <a class="nav-link text-dark small nav-scale" href="{{ route('productions.index', ['type' => 'product']) }}"><i class="fa fa-angle-double-right"></i> Товары</a>
-            <a class="nav-link text-dark small nav-scale" href="{{ route('productions.index', ['type' => 'service']) }}"><i class="fa fa-angle-double-right"></i> Услуги</a>
+            <a class="nav-link text-dark small nav-scale" href="{{ route('productions.index', ['type' => 'productions']) }}" title="Производство"><i class="fa fa-angle-double-right"></i> Производство</a>
+            <a class="nav-link text-dark small nav-scale" href="{{ route('productions.index', ['type' => 'product']) }}" title="Товары"><i class="fa fa-angle-double-right"></i> Товары</a>
+            <a class="nav-link text-dark small nav-scale" href="{{ route('productions.index', ['type' => 'service']) }}" title="Услуги"><i class="fa fa-angle-double-right"></i> Услуги</a>
         </nav>
     </div>
     <div class="col-12 col-sm-4 ml-auto col-md-3 mb-4 mb-md-0">
         <h5>Наши Контакты</h5>
         <ul class="nav flex-column">
             <li class="nav-item ">
-                <a href="" class="nav-link text-dark small d-flex ">
+                <a href="" class="nav-link text-dark small d-flex" title="Номер">
                     <i class="fas fa-phone-alt fa-lg p-1 d-none d-md-block">&nbsp;</i>&nbsp;
                     <div class="col-12">
                         <p class="mb-1 font-weight-bold">+996508900500</p>
@@ -37,7 +37,7 @@
                 </a>
             </li>
             <li class="nav-item ">
-                <a href="" class="nav-link text-dark small d-flex ">
+                <a href="" class="nav-link text-dark small d-flex" title="Адрес">
                     <i class="fas fa-map-marker-alt fa-lg p-1 d-none d-md-block">&nbsp;</i>&nbsp;
                     <div class="col-12">
                         <p class="mb-1 font-weight-bold">Кыргызстан, г. Бишкек,
@@ -46,7 +46,7 @@
                 </a>
             </li>
             <li class="nav-item ">
-                <a href="" class="nav-link text-dark small d-flex ">
+                <a href="" class="nav-link text-dark small d-flex" title="Почта">
 
                         <i class="col fas fa-envelope fa-lg p-1 d-none d-md-block">&nbsp;</i>&nbsp;
                     <div class="col-12">
