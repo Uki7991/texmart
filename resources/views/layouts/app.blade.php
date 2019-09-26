1d3d78109d65a1a4214872371ecfcbbb2b3c1b7c
Uki7991 <tilek.kubanov@gmail.com>
2019-09-26 15:11:42 +0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,8 +70,15 @@
                     @include('partials.socialnetworks')
                 </div>
             </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center">
+                    <a href="https://mount.kg" target="_blank" class="text-muted small">Made with <span
+                            class="text-danger">&hearts;</span> by Mount</a>
+                </div>
+            </div>
         </div>
     </footer>
+
 </div>
 
 <!-- Scripts -->
