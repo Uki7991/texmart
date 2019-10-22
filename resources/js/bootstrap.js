1c3ec93ef516b4835c8ebac7b3c0f73d75312dc0
Uki7991 <tilek.kubanov@gmail.com>
2019-10-22 12:25:07 +0600

    mdbootstrap js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,8 +10,8 @@ try {
     window.Popper = require('popper.js').default;
     window.$ = window.jQuery = require('jquery');
 
-    window.mdb = require('mdbootstrap');
     require('bootstrap');
+    require('mdbootstrap');
 } catch (e) {}
 
 /**
