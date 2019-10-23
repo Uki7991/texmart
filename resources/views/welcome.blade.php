530481f389f8b69e4c521f4449ba076ae757a6bf
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-23 17:06:11 +0600

    add button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,5 +14,29 @@
     @include('blocks.our_advantages')
     @include('blocks.partners')
     @include('blocks.footer')
-{{--    @include('partials.modals.10_seconds')--}}
+    {{--    @include('partials.modals.10_seconds')--}}
+    <button class="col-12 btn btn-danger rounded-pill scroll-top scale-on-hover" data-scroll="up" type="button">
+        <i class="fa fa-chevron-up text-center"></i>
+    </button>
 @endsection
+@push('scripts')
+    <script>
+        $(document).ready(function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() > 100) {
+                    $('.scroll-top').fadeIn();
+                } else {
+                    $('.scroll-top').fadeOut();
+                }
+            });
+
+            $('.scroll-top').click(function () {
+                $("html, body").animate({
+                    scrollTop: 0
+                }, 100);
+                return false;
+            });
+
+        });
+    </script>
+@endpush
