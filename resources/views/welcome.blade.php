31fd9cd7170a105011b7ba5e44e75be1ce3caeb4
Frogy <nurs.axe@gmail.com>
2019-10-23 15:47:53 +0600

    delete comment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,19 +3,16 @@
 @section('content')
     <div class="container">
         @include('blocks.side_panel')
-<<<<<<< HEAD
+
 
     </div>
-    @include('partials.modals.10_seconds')
+    {{--@include('partials.modals.10_seconds')--}}
 
 
-=======
-    </div>
     <div class="container-fluid">
         @include('blocks.middle_panel')
     </div>
     @include('blocks.partners')
     @include('blocks.footer')
 {{--    @include('partials.modals.10_seconds')--}}
->>>>>>> 58b6f2efe6893789fd09b50644e3cfafc7fdecb7
 @endsection
