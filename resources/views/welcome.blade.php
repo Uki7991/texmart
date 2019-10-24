565a6ff8903f678638e7d24c7df7d1dacd8de7be
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-24 14:48:57 +0600

    fx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,7 @@
     @include('blocks.middle_panel')
     @include('blocks.our_advantages')
     @include('blocks.partners')
-    @include('blocks.footer')
-    <button class="col-12 btn btn-danger rounded-pill scroll-top scale-on-hover" data-scroll="up" type="button">
+    <button class="col-12 btn-danger rounded-pill scroll-top scale-on-hover" data-scroll="up" type="button">
         <i class="fa fa-chevron-up text-center"></i>
     </button>
 
