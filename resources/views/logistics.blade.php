5def816f743f1324b1cc235de07c876ab57d8abd
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-29 12:28:47 +0600

    add consulting page

--- resources/views/logistics.blade.php
+++ resources/views/logistics.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
-    <section class="bg-quality-control">
+    <section class="bg-logistics">
         <div class="container h-100">
             <div class="row align-items-end h-100">
                 <div class="h1 text-white">
-                    Контроль качества
+                    Логистика
                 </div>
             </div>
         </div>
