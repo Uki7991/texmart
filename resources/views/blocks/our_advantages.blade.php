3c01bc2f4bf28b26c070829f7e37c30ab0232c63
Frogy <nurs.axe@gmail.com>
2019-10-30 15:36:34 +0600

    fuckin mobile and tablet version fuck this text KurVa Matka

--- resources/views/blocks/our_advantages.blade.php
+++ resources/views/blocks/our_advantages.blade.php
@@ -11,36 +11,36 @@
             <div class="row">
                 <div class="col-2">
 
-                    <p>
-                        <img src="{{ asset('newImages/people.png') }}" alt="">
+                    <p class="mb-1 mb-md-3">
+                        <img src="{{ asset('newImages/people.png') }}" id="preim" alt="">
                     </p>
-                    <p>
-                        <img src="{{ asset('newImages/cards-star.png') }}" class="my-3" alt="">
+                    <p class="mb-1 mb-md-3 mb-xl-3">
+                        <img src="{{ asset('newImages/cards-star.png') }}" id="preim" class="my-1 my-md-2" alt="">
 
                     </p>
-                    <p>
-                        <img src="{{ asset('newImages/like.png') }}" class="mt-1" alt="">
+                    <p class="mb-1 mb-md-3">
+                        <img src="{{ asset('newImages/like.png') }}" id="preim" class="mt-1 mt-md-0" alt="">
 
                     </p>
-                    <p>
-                        <img src="{{ asset('newImages/think.png') }}" class="my-2" alt="">
+                    <p class="mb-1 mb-md-3">
+                        <img src="{{ asset('newImages/think.png') }}" id="preim" class="my-2 my-md-0" alt="">
 
                     </p>
                 </div>
                 <div class="col-8">
-                    <p class="text-bold">
+                    <p class="text-bold pt-1 pt-md-0 mar-micro-mob">
 
                         Новая платформа ориентированная на поиске заказчиков и производителей текстильной и швейной продукции.
                     </p>
-                    <p class="text-bold">
+                    <p class="text-bold pt-2 pt-md-2 pt-xl-0 mar-micro-mob">
 
                         Рыночный проект от дешевого до качественного, дорогого продукта швейной и текстильной индустрии
                     </p>
-                    <p class="text-bold py-3">
+                    <p class="text-bold  pt-1 pt-md-0 py-md-2 py-xl-2 pad-micro-mob ">
 
                         Технология отслеживания контроля качества продукций
                     </p>
-                    <p class="text-bold">
+                    <p class="text-bold pt-2 pad-micro-mob ">
 
                         Реальные отзывы и рейтинги участников.
                     </p>
