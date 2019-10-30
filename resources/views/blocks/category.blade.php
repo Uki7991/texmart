949d0e1bf8f4df5fbf51e18960ad9a9e80f07938
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-30 14:50:41 +0600

    fx catefory blocks

--- resources/views/blocks/category.blade.php
+++ resources/views/blocks/category.blade.php
@@ -1,56 +1,56 @@
 <div class="container mt-5">
+    <h2 class="text-center" style="color: #e99b33 !important;padding-bottom: 25px">
+        Категории
+    </h2>
+    <a ></a>
     <div class="col-12 row justify-content-center">
-
-        <a class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2">
+        <a href="#" class="btn p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/women_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">Женская одежда</p>
+            <p class="card-text pt-3 text-btn text-dark">Женская одежда</p>
         </a>
-        <a class="btn peach-gradient p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2">
+        <a href="" class="btn p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/polo_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">
+            <p class="card-text pt-3 text-btn text-dark">
                 Мужская
                 одежда
             </p>
         </a>
-        <a class="btn peach-gradient p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2">
+        <a href="" class="btn p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/baby_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">
+            <p class="card-text pt-3 text-btn text-dark">
                 Детская одежда
             </p>
-        </a><a class="btn peach-gradient p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2">
+        </a>
+        <a href="" class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/specialist_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">
+            <p class="card-text pt-3 text-btn text-dark">
                 Спец одежда
             </p>
         </a>
-
-
     </div>
     <div class="col-12 row justify-content-center mt-5">
-
-        <a class="btn peach-gradient p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2 ">
+        <a href="" class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/women_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">Женская одежда</p>
+            <p class="card-text pt-3 text-btn text-dark">Женская одежда</p>
         </a>
-        <a class="btn peach-gradient p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2 ">
+        <a href="" class="btn p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/polo_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">
+            <p class="card-text pt-3 text-btn text-dark">
                 Мужская
                 одежда
             </p>
         </a>
-        <a class="btn peach-gradient p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2 ">
+        <a href="" class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/baby_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">
+            <p class="card-text pt-3 text-btn text-dark">
                 Детская одежда
             </p>
-        </a><a class="btn peach-gradient p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2">
+        </a>
+        <a href="" class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2" style="border-radius: 10px">
             <img class="img-fluid" src="{{asset('icons/specialist_dress_category.png')}}" alt="Card image cap">
-            <p class="card-text pt-3 text-btn">
+            <p class="card-text pt-3 text-btn text-dark">
                 Спец одежда
             </p>
         </a>
-
-
     </div>
 </div>
