b55cdc76444a02eabf8822b1cb56479a527d1491
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-30 16:25:19 +0600

    fx announcment

--- resources/views/blocks/announcement.blade.php
+++ resources/views/blocks/announcement.blade.php
@@ -5,26 +5,24 @@
     <div>
         <div class="card-deck">
             <div class="card card-image"
-                 style="background-image: url({{asset('img/production.png')}});background-repeat: no-repeat;background-size: cover">
-                <div class="text-white text-center d-flex align-items-center  py-5 px-4">
-                    <div>
-                        <h3 class="card-title pt-2"><strong>Производственные цеха и фабрики</strong></h3>
-                    </div>
+                 style="background-image: url({{asset('img/production.png')}});background-repeat: no-repeat;background-size: cover;height: 350px;" >
+                <div class="text-white text-center row align-items-end  pt-5 pb-4" style="height: 100%">
+                        <h3 class="card-title"><strong>Производственные цеха и фабрики</strong></h3>
                 </div>
             </div>
             <div class="card card-image"
                  style="background-image: url({{asset('img/goods.png')}});background-repeat: no-repeat;background-size: cover">
-                <div class="text-white text-center d-flex align-items-center  py-5 px-4">
+                <div class="text-white justify-content-center row align-items-end  py-5 px-4" style="height: 100%">
                     <div>
-                        <h3 class="card-title pt-2"><strong>Товары</strong></h3>
+                        <h3 class="card-title"><strong>Товары</strong></h3>
                     </div>
                 </div>
             </div>
             <div class="card card-image"
                  style="background-image: url({{asset('img/service.png')}});background-repeat: no-repeat;background-size: cover">
-                <div class="text-white text-center d-flex align-items-center  py-5 px-4">
+                <div class="text-white  row align-items-end justify-content-center py-5 px-4" style="height: 100%">
                     <div class="">
-                        <h3 class="card-title pt-2"><strong>Услуги</strong></h3>
+                        <h3 class="card-title"><strong>Услуги</strong></h3>
                     </div>
                 </div>
             </div>
