f9d45e0e3bdd13360e0eb4b3d1b6a8a1bec58ec9
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-10-31 14:46:51 +0600

    add three page

--- resources/views/blocks/category.blade.php
+++ resources/views/blocks/category.blade.php
@@ -30,24 +30,24 @@
     </div>
     <div class="col-12 row justify-content-center mt-5">
         <a href="" class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2" style="border-radius: 10px">
-            <img class="img-fluid" src="{{asset('icons/women_dress_category.png')}}" alt="Card image cap">
+            <img class="img-fluid" src="{{asset('icons/big_dress_category.png')}}" alt="Card image cap">
             <p class="card-text pt-3 text-btn text-dark">Женская одежда</p>
         </a>
         <a href="" class="btn p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-0 mt-md-2" style="border-radius: 10px">
-            <img class="img-fluid" src="{{asset('icons/polo_dress_category.png')}}" alt="Card image cap">
+            <img class="img-fluid" src="{{asset('icons/other_dress_category.png')}}" alt="Card image cap">
             <p class="card-text pt-3 text-btn text-dark">
                 Мужская
                 одежда
             </p>
         </a>
         <a href="" class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2" style="border-radius: 10px">
-            <img class="img-fluid" src="{{asset('icons/baby_dress_category.png')}}" alt="Card image cap">
+            <img class="img-fluid" src="{{asset('icons/cloth_dress_category.png')}}" alt="Card image cap">
             <p class="card-text pt-3 text-btn text-dark">
                 Детская одежда
             </p>
         </a>
         <a href="" class="btn  p-1 py-lg-2 px-lg-2 py-xl-4 px-xl-5 card-btn mx-4 mt-5 mt-md-2" style="border-radius: 10px">
-            <img class="img-fluid" src="{{asset('icons/specialist_dress_category.png')}}" alt="Card image cap">
+            <img class="img-fluid" src="{{asset('icons/service_dress_category.png')}}" alt="Card image cap">
             <p class="card-text pt-3 text-btn text-dark">
                 Спец одежда
             </p>
---
+++ resources/views/production.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('content')
+    <section class="bg-texmart-sidebar fixed-top">
+        <div class="container">
+            @include('blocks.header')
+        </div>
+    </section>
+    <div class="container">
+        <div>
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
+                <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">..</div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-3">
+
+            </div>
+            <div class="col-9">
+
+            </div>
+        </div>
+    </div>
+@endsection
