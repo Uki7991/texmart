07a06ff95bec426deed70f9c93cc1897b4f5e906
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-11-03 13:45:53 +0600

    fx announcement

--- resources/views/blocks/announcement.blade.php
+++ resources/views/blocks/announcement.blade.php
@@ -7,14 +7,14 @@
             <div class="card card-image"
                  style="background-image: url({{asset('img/production.png')}});background-repeat: no-repeat;background-size: cover;height: 350px;" >
                 <div class="text-white text-center row align-items-end  pt-5 pb-4" style="height: 100%">
-                    <a href="/production"><h3 class="card-title"><strong>Производственные цеха и фабрики</strong></h3></a>
+                    <a href="/production"><h3 class="card-title text-white"><strong>Производственные цеха и фабрики</strong></h3></a>
                 </div>
             </div>
             <div class="card card-image"
                  style="background-image: url({{asset('img/goods.png')}});background-repeat: no-repeat;background-size: cover">
                 <div class="text-white justify-content-center row align-items-end  py-5 px-4" style="height: 100%">
                     <div>
-                        <a href=""><h3 class="card-title"><strong>Товары</strong></h3></a>
+                        <a href=""><h3 class="card-title text-white"><strong>Товары</strong></h3></a>
                     </div>
                 </div>
             </div>
@@ -22,7 +22,7 @@
                  style="background-image: url({{asset('img/service.png')}});background-repeat: no-repeat;background-size: cover">
                 <div class="text-white  row align-items-end justify-content-center py-5 px-4" style="height: 100%">
                     <div class="">
-                        <a href=""><h3 class="card-title"><strong>Услуги</strong></h3></a>
+                        <a href=""><h3 class="card-title text-white"><strong>Услуги</strong></h3></a>
                     </div>
                 </div>
             </div>
