a35fb93243973029cb594b5b3f26f997bdb74291
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-11-04 17:24:16 +0600

    add fonts

--- resources/views/blocks/middle_panel.blade.php
+++ resources/views/blocks/middle_panel.blade.php
@@ -1,5 +1,5 @@
-<div class="container-fluid position-relative vh-100" style="overflow:hidden;">
-    <div class="row" style="left: 0; top: 0;">
+<div class="container-fluid position-relative mt-5 mt-xl-0 vh-100" style="overflow:hidden;">
+    <div class="row pl-5" style="left: 0; top: 0;">
         <div class="col-12 position-absolute p-0">
             {{--new carousel but this carousel is slick.js carouse--}}
             <div class="" id="background-carousel" style="z-index: 0;">
@@ -9,17 +9,17 @@
             </div>
             {{--end carousel--}}
         </div>
-        <div class="col-12 col-lg-6 position-relative min-vh-100">
+        <div class="col-12 pl-5 col-lg-6 position-relative min-vh-100">
             <div class="backdrop h-100"></div>
             <div class="row h-100 position-relative">
                 <div class="col-12">
-                    <div class="pt-4 mb-5 position-relative text-center">
+                    <div class="pt-4 mb-5 position-relative text-left pl-5 ">
                         <img src="{{asset('img/logo3.png')}}" class="img-fluid" alt="logo">
                     </div>
                 </div>
                 {{--owl carousel for text--}}
-                <div class="col-12">
-                    <div class="text-center">
+                <div class="col-12 pl-5">
+                    <div class="text-left">
                         <div id="owl-text" class="owl-carousel owl-theme">
                             <div class="item text-white">
                                 <p class="h3">Первая интернерт платформа швейной и <br>текстильной промышленности Кыргызстана</p>
@@ -35,39 +35,49 @@
                 </div>
                 {{--end carousel--}}
                 {{-- button for create new advertisement--}}
-                <div class="add_announcement col-12 position-relative text-center">
+                <div class="add_announcement col-12 position-relative text-left pl-5">
                     <a href="" class="text-white" style="border-radius: 10px;border:1px solid #d0d7dd; padding:8px 30px;">Добавить объявление</a>
                 </div>
 
-                <div class="text-center col-12 position-relative mt-5">
+                <div class="col-12 pl-5 position-relative mt-5">
                     <ul class="list-inline text-light">
-                        <li class="list-inline-item">
+                        <li class="list-inline-item text-center">
                             <h4 class="mx-5 folder-sprite waves-effect waves-light "></h4>
-                            Единая база <br> производителей
+                            <p>
+                                Единая база <br> производителей
+                            </p>
                         </li>
-                        <li class="list-inline-item">
+                        <li class="list-inline-item text-center">
                             <h4 class="mx-5 mb-4 medal-sprite waves-effect waves-light"></h4>
-                            <span class="pb-3">Оценка качества</span>
+                            <p class="pb-3">Оценка качества</p>
                         </li>
-                        <li class="list-inline-item">
+                        <li class="list-inline-item text-center">
                             <h4 class="mx-5 cloth-sprite waves-effect waves-light"></h4>
-                            Поставка тканей <br>
-                            и фурнитуры
+                            <p>
+                                Поставка тканей <br>
+                                и фурнитуры
+                            </p>
                         </li>
                     </ul>
                     <ul class="list-inline text-light">
-                        <li class="list-inline-item">
+                        <li class="list-inline-item text-center">
                             <h4 class="mx-5 production-sprite waves-effect waves-light"></h4>
-                            Производство <br> одежды  в КР
+                            <p>
+                                Производство <br> одежды  в КР
+                            </p>
                         </li>
-                        <li class="list-inline-item">
+                        <li class="list-inline-item text-center">
                             <h4 class="mxmx-5 -3 box-sprite waves-effect waves-light"></h4>
-                            Доставка
+                            <p>
+                                Доставка
+                            </p>
                         </li>
-                        <li class="list-inline-item">
+                        <li class="list-inline-item text-center">
                             <h4 class="mx-5 document-sprite waves-effect waves-light"></h4>
-                            Оформление <br>
-                            документов
+                            <p>
+                                Оформление <br>
+                                документов
+                            </p>
                         </li>
                     </ul>
                 </div>
