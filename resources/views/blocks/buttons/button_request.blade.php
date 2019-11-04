193477d555fa764bf9a8f2938655ff1a2abb6558
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-11-04 16:32:00 +0600

    fx button model

--- resources/views/blocks/buttons/button_request.blade.php
+++ resources/views/blocks/buttons/button_request.blade.php
@@ -15,9 +15,13 @@
                     <label data-error="wrong" data-success="right" for="form34">Ваше имя</label>
                 </div>
                 <div class="md-form mb-5">
-                    <i class="fas fa-phone-alt prefix grey-text"></i>
-                    <input type="tel" id="form29" class="form-control validate">
-                    <label data-error="wrong" data-success="right" for="form29">Ваш телефон</label>
+{{--                    <i class="fas fa-phone-alt prefix grey-text"></i>--}}
+                    <input type="text" class="form-control @error('phone') is-invalid @enderror"  id="phone-number_2">
+{{--                    <input type="hidden" name="code">--}}
+{{--                    <input type="text"--}}
+{{--                           class="form-control rounded-pill shadow-sm @error('phone') is-invalid @enderror"--}}
+{{--                           name="phone" required autocomplete="phone"--}}
+{{--                           id="phone-number_2">--}}
                 </div>
                 <div class="md-form">
                     <i class="fas fa-pencil prefix grey-text"></i>
@@ -26,9 +30,53 @@
                 </div>
             </div>
             <div class="modal-footer d-flex justify-content-center">
-                <button class="btn btn-unique">Отправить <i class="fas fa-paper-plane-o ml-1"></i></button>
+                <button class="btn btn-texmart-orange text-white">Отправить <i class="fas fa-paper-plane-o ml-1"></i></button>
             </div>
         </div>
     </div>
 </div>
+@push('styles')
+    <link  rel="stylesheet"  href = "{{asset("css/intlTelInput.min.css")}}">
+@endpush
+@push('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.js"></script>
+    <script src="{{ asset('js/intlTelInput-jquery.min.js') }}"></script>
+    <script>
+        /* INITIALIZE BOTH INPUTS WITH THE intlTelInput FEATURE*/
 
+        $("#phone-number_2").intlTelInput({
+            initialCountry: "kg",
+            preferredCountries: ["ru", "kg", "kz"],
+            separateDialCode: true,
+            excludeCountries: ["xk"],
+            geoIpLookup: function (callback) {
+                $.get('https://ipinfo.io', function () {
+                }, "jsonp").always(function (resp) {
+                    var countryCode = (resp && resp.country) ? resp.country : "";
+                    console.log(countryCode);
+                    callback(countryCode);
+                });
+            },
+            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/11.0.14/js/utils.js"
+        });
+        $('#phone-number_2').on('focus', function(e) {
+            let input = $(e.currentTarget);
+            let code = input.siblings('.iti__flag-container').find('.iti__selected-dial-code').html();
+            input.parent().siblings('input[name="code"]').val(code);
+            var $this = $(this),
+                // Get active country's phone number format from input placeholder attribute
+                activePlaceholder = $this.attr('placeholder'),
+                // Convert placeholder as exploitable mask by replacing all 1-9 numbers with 0s
+                newMask = activePlaceholder.replace(/[1-9]/g, "0");
+            // console.log(activePlaceholder + ' => ' + newMask);
+
+            // Init new mask for focused input
+            $this.mask(newMask);
+        });
+
+        $('#phone-number_2').on('countrychange', (e, c) => {
+            let $this = $(e.currentTarget);
+            $this.removeAttr('maxlength');
+        });
+    </script>
+@endpush
--- resources/views/blocks/category.blade.php
+++ resources/views/blocks/category.blade.php
@@ -5,19 +5,19 @@
     <div class="row justify-content-center">
         <div class="row col-10">
             <div class="col-6 col-md-3 mb-5">
-                <a href="#" class="card py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
+                <a href="#" class="card shadow py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
                     <img class="w-25 mx-auto" src="{{ asset('icons/women_dress_category.png') }}" alt="Card image cap">
                     <p class="card-title mt-4 text-dark text-center">Женская одежда</p>
                 </a>
             </div>
             <div class="col-6 col-md-3 mb-5">
-                <a href="#" class="card py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
+                <a href="#" class="card shadow py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
                     <img class="w-25 mx-auto" src="{{ asset('icons/polo_dress_category.png') }}" alt="Card image cap">
                     <p class="card-title mt-4 text-dark text-center">Мужская одежда</p>
                 </a>
             </div>
             <div class="col-6 col-md-3 mb-5">
-                <a href="#" class="card py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
+                <a href="#" class="card shadow py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
                     <img class="w-25 mx-auto" src="{{ asset('icons/baby_dress_category.png') }}" alt="Card image cap">
                     <p class="card-title mt-4 text-dark text-center">Детская одежда</p>
                 </a>
@@ -42,19 +42,16 @@
             </div>
             <div class="col-6 col-md-3 mb-5">
                 <a href="#" class="card py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
-
                     <img class="w-25 mx-auto" src="{{ asset('icons/cloth_dress_category.png') }}" alt="Card image cap">
                     <p class="card-title mt-4 text-dark text-center">Ткань, фурнитура</p>
                 </a>
             </div>
             <div class="col-6 col-md-3 mb-5">
                 <a href="#" class="card py-2 py-md-4 py-lg-5" style="border-radius: 10px;">
-
                     <img class="w-25 mx-auto" src="{{ asset('icons/service_dress_category.png') }}" alt="Card image cap">
                     <p class="card-title mt-4 text-dark text-center">Услуги</p>
                 </a>
             </div>
         </div>
     </div>
-
 </div>
