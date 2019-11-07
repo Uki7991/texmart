776405566d5a8253f5bcb1964a12fc334cf8d6dc
Frogy <nurs.axe@gmail.com>
2019-11-07 11:59:49 +0600

    adaptive our_advantages block

--- resources/views/blocks/our_advantages.blade.php
+++ resources/views/blocks/our_advantages.blade.php
@@ -5,55 +5,45 @@
                 Наши преимущества
             </h2>
         </div>
-        <div class="col-sm-11 col-md-6 py-5">
+        <div class="col-sm-11 col-lg-6 py-5">
+            <div class="media mt-4 px-1 text-md-left">
+                <div class="sprite-community d-flex mr-3" alt="Generic placeholder image"></div>
 
+                <div class="media-body">
 
-            <div class="row">
-                <div class="col-2">
-
-                    <p class="mb-1 mb-md-3">
-                        <img src="{{ asset('newImages/people.png') }}" id="preim" alt="">
-                    </p>
-                    <p class="mb-1 mb-md-3 mb-xl-3">
-                        <img src="{{ asset('newImages/cards-star.png') }}" id="preim" class="my-1 my-md-2" alt="">
-                    </p>
-                    <div class="sprite-community"></div>
-                    <p class="mb-1 mb-md-3">
-                        <img src="{{ asset('newImages/like.png') }}" id="preim" class="mt-1 mt-md-0" alt="">
-
-                    </p>
-                    <p class="mb-1 mb-md-3">
-                        <img src="{{ asset('newImages/think.png') }}" id="preim" class="my-2 my-md-0" alt="">
-
-                    </p>
-                </div>
-                <div class="col-8">
-                    <p class="text-bold pt-1 pt-md-0 mar-micro-mob">
-
-                        Новая платформа ориентированная на поиске заказчиков и производителей текстильной и швейной продукции.
-                    </p>
-                    <p class="text-bold pt-2 pt-md-2 pt-xl-0 mar-micro-mob">
-
-                        Рыночный проект от дешевого до качественного, дорогого продукта швейной и текстильной индустрии
-                    </p>
-                    <p class="text-bold  pt-1 pt-md-0 py-md-2 py-xl-2 pad-micro-mob ">
-
-                        Технология отслеживания контроля качества продукций
-                    </p>
-                    <p class="text-bold pt-2 pad-micro-mob ">
-
-                        Реальные отзывы и рейтинги участников.
-                    </p>
+                    Новая платформа ориентированная на поиске заказчиков и производителей текстильной и швейной продукции.
                 </div>
             </div>
+            <div class="media mt-4 px-1 text-md-left">
+                <div class="sprite-cardStar d-flex mr-3" alt="Generic placeholder image"></div>
+                <div class="media-body">
+
+                    Рыночный проект от дешевого до качественного, дорогого продукта швейной и текстильной индустрии
+                </div>
+            </div>
+            <div class="media mt-4 px-1 text-md-left">
+                <div class="sprite-like d-flex mr-3" alt="Generic placeholder image"></div>
+                <div class="media-body">
+
+                    Технология отслеживания контроля качества продукций
+                </div>
+            </div><div class="media mt-4 px-1 text-md-left">
+                <div class="sprite-disOrLike d-flex mr-3" alt="Generic placeholder image"></div>
+                <div class="media-body">
+
+                    Реальные отзывы и рейтинги участников.
+                </div>
+            </div>
+
+
         </div>
-        <div class="col-6 py-5 bg-img">
+        <div class="col-12 col-lg-6 py-5 bg-img">
 
             {{--<iframe  frameborder="0" allow="></iframe>--}}
 
             <a class="fancybox-media" href="https://www.youtube.com/watch?time_continue=3&v=xTYkmWnwLvg">
                 <div class="position-relative">
-                    <img src="{{ asset('img/video.png') }}" class="img-fluid   position-relative" alt="">
+                    <img src="{{ asset('img/video.png') }}" class="img-fluid w-100 position-relative" alt="">
                     <img src="{{asset('img/youtube(2).png')}}" class="youtube  position-absolute" style="left: 50%; top: 50%; transform: translate(-50%, -50%); " alt="">
                 </div>
             </a>
