cbf8d2a40e8a204e37ab875bc174b2d6130253e2
Frogy <nurs.axe@gmail.com>
2019-11-12 17:28:38 +0600

    button midle panel fix super ultra

--- resources/views/blocks/middle_panel.blade.php
+++ resources/views/blocks/middle_panel.blade.php
@@ -27,10 +27,10 @@
                 {{--end carousel--}}
                 {{-- button for create new advertisement--}}
                 <div class="add_announcement col-12 row position-relative text-left pl-lg-5 pt-3">
-                    <div class="col-12 col-md-4 col-lg-6 mb-3 mb-md-0">
+                    <div class="col-12 col-xl-6 mb-3 mb-xl-0">
                         <a href="http://texmart/profile/production?type=productions" class="text-white small" style="border-radius: 10px;border:1px solid #d0d7dd; padding:8px 30px;">Добавить объявление</a>
                     </div>
-                    <div class="col-12 col-md-4 col-lg-6">
+                    <div class="col-12 col-xl-6">
                         <a href="#form-review" class="text-white btn-sm btn-texmart-orange" style="border-radius: 10px;padding:9px 32px;">Предложить заказ</a>
                     </div>
                 </div>
