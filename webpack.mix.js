4a270db61f8d45744beb574c13443f5ed5cc8e5b
Uki7991 <tilek.kubanov@gmail.com>
2019-11-16 23:48:08 +0600

    fx app.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,20 +12,9 @@ require('laravel-mix-purgecss');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .purgeCss({
-        enabled: true
-    });
+    .sass('resources/sass/app.scss', 'public/css');
 
-mix.styles(['public/css/slick-theme.css', 'public/css/slick.css'], 'public/css/slick.min.css').purgeCss({
-    enabled: true
-});
-mix.styles(['public/css/main.css'], 'public/css/main.min.css').purgeCss({
-    enabled: true
-});
-mix.styles(['public/css/mmenu.css'], 'public/css/mmenu.min.css').purgeCss({
-    enabled: true
-});
-mix.styles(['public/css/jquery.rateyo.css'], 'public/css/jquery.rateyo.min.css').purgeCss({
-    enabled: true
-});
+mix.styles(['public/css/slick-theme.css', 'public/css/slick.css'], 'public/css/slick.min.css');
+mix.styles(['public/css/main.css'], 'public/css/main.min.css');
+mix.styles(['public/css/mmenu.css'], 'public/css/mmenu.min.css');
+mix.styles(['public/css/jquery.rateyo.css'], 'public/css/jquery.rateyo.min.css');
