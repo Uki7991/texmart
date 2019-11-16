3e0345d94b673e89212bbd1360a6e4f076711a00
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2019-11-17 00:50:19 +0600

    fx profile

--- resources/views/profile/partials/sidebar.blade.php
+++ resources/views/profile/partials/sidebar.blade.php
@@ -7,12 +7,14 @@
     {{--    <a href="{{ route('admin.category.datatable') }}" class="list-group-item list-group-item-action {{ request()->is('admin/category*') ? 'active' : '' }}">{{ __('Категории') }}</a>--}}
     {{--    <a href="{{ route('admin.product.datatable') }}" class="list-group-item list-group-item-action {{ request()->is('admin/product*') ? 'active' : '' }}">{{ __('Товары') }}</a>--}}
     @if(auth()->user()->role_id == 5 || auth()->user()->role_id == 1)
-        <a href="#addrequest" id="accordion" data-toggle="collapse" class="list-group-item list-group-item-action step4"  aria-expanded="false">Подать объявления</a>
-        <div class="collapse step5" id="addrequest">
+        <a href="#addrequest" id="accordion" data-toggle="collapse" class="list-group-item list-group-item-action step4"  aria-expanded="false">Подать объявления
+            <i class="fas fa-angle-down rotate-icon" style="position: absolute;top: .8rem;right: 0;margin-right: 1.25rem;"></i>
+        </a>
+        <div class="collapse step5 {{ request()->query('type') == 'service' || request()->query('type') == 'productions' || request()->query('type') == 'product' ? "show" : "" }}" id="addrequest">
 
-            <a href="{{ route('profile.production.index', ['type' => 'productions']) }}" class="list-group-item list-group-item-action step6 {{ request()->query('type') == 'productions' ? 'active' : '' }}">Цеха</a>
-            <a href="{{ route('profile.production.index', ['type' => 'service']) }}" class="list-group-item list-group-item-action step7 {{ request()->query('type') == 'service' ? 'active' : '' }}">Услуги</a>
-            <a href="{{ route('profile.production.index', ['type' => 'product']) }}" class="list-group-item list-group-item-action step8 {{ request()->query('type') == 'product' ? 'active' : '' }}">Товары</a>
+            <a href="{{ route('profile.production.index', ['type' => 'productions']) }}" class="list-group-item list-group-item-action step6  {{ request()->query('type') == 'productions' ? 'active' : 'grey lighten-2' }}" style="padding-left: 15%;">Цеха</a>
+            <a href="{{ route('profile.production.index', ['type' => 'service']) }}" class="list-group-item list-group-item-action step7 {{ request()->query('type') == 'service' ? 'active' : 'grey lighten-2' }}" style="padding-left: 15%">Услуги</a>
+            <a href="{{ route('profile.production.index', ['type' => 'product']) }}" class="list-group-item list-group-item-action step8 {{ request()->query('type') == 'product' ? 'active' : 'grey lighten-2' }}" style="padding-left: 15%">Товары</a>
         </div>
     @endif
 
@@ -22,3 +24,13 @@
 <form action="{{ route('logout') }}" method="POST" class="d-none logout-form">
     @csrf
 </form>
+@push('scripts')
+    <script>
+        $('.collapse').on('show.bs.collapse', function () {
+            $('.fa-angle-down').addClass('fa-rotate-180');
+        });
+        $('.collapse').on('hide.bs.collapse', function () {
+            $('.fa-angle-down').removeClass('fa-rotate-180');
+        });
+    </script>
+@endpush
