04bef6b7f8f4d7b2469553026a7af6b7534382ab
Uki7991 <tilek.kubanov@gmail.com>
2020-02-20 13:52:59 +0600

    Update serviceworker.js

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -48,6 +48,7 @@ self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
+                console.log(response);
                 return response || fetch(event.request);
             })
             .catch(() => {
